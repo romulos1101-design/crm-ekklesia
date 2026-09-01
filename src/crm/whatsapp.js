dc647c79dda8b164407db5d293003e2b4042ca23
romulos1101-design <romulos1101@gmail.com>
2026-09-01 04:17:00 -0300

    Delete src directory

--- src/crm/whatsapp.js
+++
@@ -1,6 +0,0 @@
-// src/crm/whatsapp.js - 100% API OFICIAL META
-export async function enviarTemplate(contato, templateId, phoneId){
-  console.log(`[WHATSAPP] Enviando ${templateId} para ${contato.telefone} via ${phoneId}`);
-  return true;
-}
-export async function getTaxaBloqueio(){ return 0; }
